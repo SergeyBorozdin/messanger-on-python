95ee27d13801447e246e44d804715907e3aa88e5
Sergey Borozdin <borozdinsm@yandex.ru>
2022-10-02 00:05:47 +0300

    ad func save and send messages

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
-import json #импорт библиотеки для работы с json
+import json  # импорт библиотеки для работы с json
 
 app = Flask(__name__)  # создали сервер
 all_messages = []  # переменная где храниться вся история чата
 DB_FILE = 'db.json'
 
 
-def load_messanges(): #функция для загрузки сообщений из файла db.json
+def load_messanges():  # функция для загрузки сообщений из файла db.json
     with open(DB_FILE, 'r') as json_file:
         data = json.load(json_file)
-    return  data['messages']
+    return data['messages']
+
+
+def save_messages(): # функ сохраняет историю в json файл
+    data = {'messages': all_messages}
+    with open(DB_FILE, 'w') as json_file:
+        json.dump(data, json_file)
+
 
 @app.route('/')  # @ аннотация указывает на какую страницу должна вести ссылка
 def index_page():
     return 'Hello word!'
 
-@app.route('/chat') # ссылка на дисплей чата
-def dispay_chart(): # отобразить наш чат по шаблону из  templates/form.htm
+
+@app.route('/chat')  # ссылка на дисплей чата
+def dispay_chart():  # отобразить наш чат по шаблону из  templates/form.htm
     return render_template('form.html')
 
 
@@ -43,12 +51,20 @@ def print_all_message(message_list):
         print_message(msg)
 
 
-add_message('Влад', 'Привет как дела?')
-add_message('Петр', 'Привет Влад, я отлично')
+@app.route('/send_message') # http://127.0.0.1:5000/send_message?name=Vlad&text=Hello
+def send_message(): # функ которая отправляет сообщение
+    sender = request.args['name']
+    text = request.args['text']
+    add_message(sender, text)
+
+
+# add_message('Влад', 'Привет как дела?')
+# add_message('Петр', 'Привет Влад, я отлично')
 
 
 @app.route('/get_messages')  # указываем ссылку по которой получаем все сообщения
 def get_messages():  # получаем списком все сообщения
     return {'messages': all_messages}
+
 
 app.run()  # запустили сервер
